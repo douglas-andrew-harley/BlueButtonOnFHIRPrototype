540da45820ea2f6d7621e5ac6e55a82868b78468
Douglas Harley <douglas.andrew.harley@gmail.com>
2016-04-02 13:39:36 -0400

    retrieving EOBs for patient

--- app/src/main/java/net/dfunkt/bluebuttononfhirprototype/MainActivity.java
+++ app/src/main/java/net/dfunkt/bluebuttononfhirprototype/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import com.marvinlabs.widget.floatinglabel.edittext.FloatingLabelEditText;
 
+import org.hl7.fhir.instance.model.ExplanationOfBenefit;
 import org.hl7.fhir.instance.model.HumanName;
 import org.hl7.fhir.instance.model.Patient;
 
@@ -30,10 +31,15 @@ public class MainActivity extends AppCompatActivity {
     private static final IGenericClient RESTFUL_CLIENT = FHIR_DSTU_2_CONTEXT.newRestfulGenericClient(SERVER_BASE_URL);
     private static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName(), null);
 
+    private String patientId;
+    String string = "";
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)MainActivity.this.findViewById(R.id.patient_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
@@ -42,14 +48,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                string = "";
                 new AsyncTask<Void, Void, Patient>() {
                     private ProgressDialog progressDialog;
-                    private String patientId;
                     @Override
                     protected void onPreExecute() {
                         super.onPreExecute();
                         patientId = ((FloatingLabelEditText)findViewById(R.id.patient_id_edit)).getInputWidgetText().toString();
-                        progressDialog = ProgressDialog.show(MainActivity.this, null, "Retrieving data from server", true, false);
+                        progressDialog = ProgressDialog.show(MainActivity.this, null, "Retrieving patient data from server", true, false);
                     }
                     @Override
                     protected Patient doInBackground(Void... params) {
@@ -77,19 +83,54 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "No patient with ID '" + patientId + "' found", Toast.LENGTH_LONG).show();
                             textView.setVisibility(View.INVISIBLE);
                         } else {
-                            String string = "";
                             for(HumanName name : patient.getName()) {
                                 string += "GIVEN NAME: " + name.getGiven().get(0) + "\n";
                                 string += "FAMILY NAME: " + name.getFamily().get(0) + "\n";
                             }
                             string += "BIRTH DATE: " + patient.getBirthDate() + "\n";
                             LOGGER.info("string =" + string + "=");
-                            textView.setText(string);
-                            textView.setVisibility(View.VISIBLE);
+                            getExplanationOfBenefitsBundle(patient);
                         }
                     }
                 }.execute();
             }
         });
+    }
+
+    private void getExplanationOfBenefitsBundle(final Patient patient) {
+        new AsyncTask<Void, Void, org.hl7.fhir.instance.model.Bundle>() {
+            private ProgressDialog progressDialog;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(MainActivity.this, null, "Retrieving EOB data from server", true, false);
+            }
+            @Override
+            protected org.hl7.fhir.instance.model.Bundle doInBackground(Void... params) {
+                org.hl7.fhir.instance.model.Bundle results = null;
+                String url = "ExplanationOfBenefit?patient=Patient/" + patientId;
+                LOGGER.info("url =" + url + "=");
+                try {
+                    results = RESTFUL_CLIENT
+                            .search()
+                            .byUrl(url)
+                            .returnBundle(org.hl7.fhir.instance.model.Bundle.class)
+                            .execute();
+                    string += (null == results || null == results.getEntry()) ? "No EOBs returned." : "EOBs returned: " + results.getEntry().size();
+                } catch (Exception exception) {
+                    LOGGER.severe(exception.toString());
+                }
+                LOGGER.info("results =" + results + "=");
+                return results;
+            }
+
+            @Override
+            protected void onPostExecute(org.hl7.fhir.instance.model.Bundle bundle) {
+                super.onPostExecute(bundle);
+                progressDialog.dismiss();
+                textView.setText(string);
+                textView.setVisibility(View.VISIBLE);
+            }
+        }.execute();
     }
 }
